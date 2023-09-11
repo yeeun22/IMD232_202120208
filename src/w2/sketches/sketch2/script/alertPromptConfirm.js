c29ec44474abf51d1e4b8a84be3263806b407fb1
yeeun22 <128441724+yeeun22@users.noreply.github.com>
2023-09-11 16:02:15 +0900

    9월11일 쉅

---
+++ src/w2/sketches/sketch2/script/alertPromptConfirm.js
@@ -0,0 +1,3 @@
+alert('안녕하세욥!');
+let userName = prompt('키미노...나마에와?', '홍길동');
+confirm('헤에.. ' + userName + '쿤/쨩?');
---
+++ src/w2/sketches/sketch4/script/sketch.js
@@ -0,0 +1,9 @@
+// html의 head와 body의 관계
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255);
+  circle(mouseX, mouseY, 100);
+}
