ec5af52c0b016ed28b283501029aee324016f226
yeeun22 <128441724+yeeun22@users.noreply.github.com>
2023-09-18 20:57:36 +0900

    심화 완

--- src/w2/assignment/advance/script/sketch.js
+++ src/w2/assignment/advance/script/sketch.js
@@ -1,62 +1,105 @@
 function setup() {
+  setCanvasContainer('canvas');
   background(255);
 }
 
 function draw() {
   background('#E4C8A1');
-  createCanvas();
   //   바닥
   fill('#453A40');
-  rect(0, 450, 10000, 80);
+  noStroke();
+  rect(0, height * 0.93, width, height * 0.1);
 
   //   조명
   noStroke();
   fill(255, 30);
-  ellipse(465, 100, 180);
+  ellipse(width / 2, height * 0.2, height * 0.4);
   fill(255, 50);
-  ellipse(465, 100, 130);
+  ellipse(width / 2, height * 0.2, height * 0.3);
   fill(255, 60);
-  ellipse(465, 100, 110);
+  ellipse(width / 2, height * 0.2, height * 0.22);
   fill(255);
-  ellipse(465, 100, 75);
+  ellipse(width / 2, height * 0.2, height * 0.18);
   fill(255, 80);
-  ellipse(560, 60, 50);
-  ellipse(340, 180, 30);
+  ellipse(width * 0.7, height * 0.1, height * 0.12);
+  ellipse(width * 0.35, height * 0.38, height * 0.07);
+  ellipse(width * 0.3, height * 0.43, height * 0.04);
   stroke(50);
-  line(465, 0, 465, 62.5);
+  line(width / 2, 0, width / 2, height * 0.11);
 
   // 벽난로
   noStroke();
   fill('#132823');
-  rect(350, 250, 240, 200);
+  rect(width * 0.36, height * 0.52, width * 0.26, height * 0.41);
+  // height * 0.41 = 약 200
   fill('#2D433F');
-  rect(325, 240, 290, 35);
+  rect(width * 0.34, height * 0.5, width * 0.3, height * 0.07);
   fill(255);
   noStroke();
-  rect(400, 300, 140, 150, 999, 999, 0, 0);
+  rect(
+    width * 0.42,
+    height * 0.62,
+    width * 0.14,
+    height * 0.31,
+    999,
+    999,
+    0,
+    0
+  );
   fill('#88171A');
-  rect(420, 320, 100, 130, 999, 999, 0, 0);
+  rect(
+    width * 0.435,
+    height * 0.65,
+    width * 0.11,
+    height * 0.28,
+    999,
+    999,
+    0,
+    0
+  );
 
   //   고양이
   //   (다리)
   fill(0);
-  rect(640, 390, 15, 73, 99);
-  rect(655, 390, 15, 73, 99);
+  rect(width * 0.68, height * 0.8, width * 0.015, height * 0.16, 99);
+  rect(width * 0.695, height * 0.8, width * 0.015, height * 0.16, 99);
   //   (얼굴)
-  ellipse(654, 380, 55, 48);
+  ellipse(width * 0.694, height * 0.8, width * 0.06, height * 0.11);
+  //   (귀)
+  stroke(0);
+  triangle(
+    width * 0.67,
+    height * 0.77,
+    width * 0.682,
+    height * 0.72,
+    width * 0.7,
+    height * 0.77
+  );
+  triangle(
+    width * 0.68,
+    height * 0.78,
+    width * 0.702,
+    height * 0.72,
+    width * 0.72,
+    height * 0.78
+  );
+
+  // (몸)
+  triangle(
+    width * 0.7,
+    height * 0.91,
+    width * 0.7,
+    height * 0.78,
+    width * 0.74,
+    height * 0.91
+  );
+  rect(width * 0.7, height * 0.91, width * 0.07, height * 0.025, 99);
   //   (눈)
   fill(255);
   noStroke();
-  ellipse(652, 380, 17, 18);
-  ellipse(665, 380, 17, 18);
+  ellipse(width * 0.69, height * 0.8, width * 0.02, height * 0.042);
+  ellipse(width * 0.705, height * 0.8, width * 0.02, height * 0.042);
   fill(0);
-  ellipse(654, 380, 10);
-  ellipse(667, 380, 10);
-  //   (귀)
-  stroke(0);
-  triangle(638, 370, 650, 350, 662, 370);
-  triangle(650, 370, 662, 350, 674, 370);
-  // (몸)
-  triangle(666, 450, 666, 390, 710, 450);
-  rect(660, 438, 80, 13, 99);
+  ellipse(width * 0.693, height * 0.8, width * 0.012);
+  ellipse(width * 0.707, height * 0.8, width * 0.012);
 }
