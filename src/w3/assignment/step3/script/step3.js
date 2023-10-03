29693c60bbe97539b2c80feb2fe905b282b48d22
yeeun22 <128441724+yeeun22@users.noreply.github.com>
2023-10-04 02:06:14 +0900

    w3_assignmnet_step3

---
+++ src/w3/assignment/step3/script/step3.js
@@ -0,0 +1,30 @@
+let mover;
+let mVec;
+
+function setup() {
+  setCanvasContainer('canvasHere', 3, 2, true);
+
+  mover = new Mover(width / 2, height / 2, 25);
+  mVec = createVector();
+
+  background('white');
+}
+
+function draw() {
+  mVec.set(mouseX, mouseY);
+  let towardMouseVec = p5.Vector.sub(mVec, mover.pos);
+  towardMouseVec.setMag(0.1);
+  if (mouseIsPressed) {
+    towardMouseVec.mult(-1);
+  }
+  mover.setAcc(towardMouseVec);
+  mover.update();
+
+  background('white');
+  mover.display();
+  mover.displayVectors();
+
+  stroke(0);
+  strokeWeight(1);
+  line(0, 0, mouseX - mover.pos.x, mouseY - mover.pos.y);
+}
