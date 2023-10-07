32bafd8c4db02a86296b1b993705c3aed2ab620b
yeeun22 <128441724+yeeun22@users.noreply.github.com>
2023-10-08 01:47:21 +0900

    W4 - sketch 2

---
+++ src/w4/sketches/sketch2/script/MoverNoMass.js
@@ -0,0 +1,69 @@
+class MoverNoMass {
+  constructor(x, y, r) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.rad = r;
+  }
+
+  addAcc(accInput) {
+    this.acc.add(accInput);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  checkEdges() {
+    if (this.pos.x < 0) {
+      this.pos.x -= 0;
+      this.pos.x *= -1;
+      this.pos.x += 0;
+      this.vel.x *= -1;
+    } else if (this.pos.x > width - 1) {
+      this.pos.x -= width - 1;
+      this.pos.x *= -1;
+      this.pos.x += width - 1;
+      this.vel.x *= -1;
+    }
+    if (this.pos.y < 0) {
+      this.pos.y -= 0;
+      this.pos.y *= -1;
+      this.pos.y += 0;
+      this.vel.y *= -1;
+    } else if (this.pos.y > height - 1) {
+      this.pos.y -= height - 1;
+      this.pos.y *= -1;
+      this.pos.y += height - 1;
+      this.vel.y *= -1;
+    }
+  }
+
+  display() {
+    stroke('black');
+    strokeWeight(1);
+    fill(200);
+    ellipse(this.pos.x, this.pos.y, 2 * this.rad);
+  }
+
+  displayVector() {
+    stroke('red');
+    strokeWeight(2);
+    line(
+      this.pos.x,
+      this.pos.y,
+      this.pos.x + this.vel.x * 10,
+      this.pos.y + this.vel.y * 10
+    );
+    stroke('blue');
+    strokeWeight(2);
+    line(
+      this.pos.x,
+      this.pos.y,
+      this.pos.x + this.acc.x * 100,
+      this.pos.y + this.acc.y * 100
+    );
+  }
+}
