25e4358d820e04db3266fc374de738e19822c5c8
yeeun22 <128441724+yeeun22@users.noreply.github.com>
2023-10-14 08:07:37 +0900

    step1

---
+++ src/w4/assignment/step1/script/sketch.js
@@ -0,0 +1,47 @@
+let bodies = [];
+let bodyNum = 35;
+
+let G = 0.2;
+
+let showVector = false;
+
+function setup() {
+  setCanvasContainer('canvas', 3, 2, true);
+  reset();
+}
+
+function draw() {
+  background(255);
+
+  for (let i = 0; i < bodyNum; i++) {
+    for (let j = 0; j < bodyNum; j++) {
+      if (i !== j) {
+        let forceForJ = bodies[i].attract(bodies[j]);
+        bodies[j].applyForce(forceForJ);
+      }
+    }
+    bodies[i].update();
+    bodies[i].display();
+    if (showVector) {
+      bodies[i].displayVectors();
+    }
+  }
+}
+
+function mousePressed() {
+  if (isMouseInsideCanvas()) {
+    reset();
+  }
+}
+
+function reset() {
+  for (let i = 0; i < bodyNum; i++) {
+    bodies[i] = new Body(random(width), random(height), random(16, 100));
+  }
+}
+
+function keyPressed() {
+  if (key === 's' || key === 'S') {
+    showVector = !showVector;
+  }
+}
