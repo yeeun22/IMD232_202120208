4d72cf08a9b185cf74c4a108cc80f250e1dd9134
yeeun22 <128441724+yeeun22@users.noreply.github.com>
2023-11-06 13:43:34 +0900

    과제 링크 완

---
+++ src/w9/atClass/stp1/script/Vehicle.js
@@ -0,0 +1,58 @@
+class Vehicle {
+  constructor(x, y, rad, speedMx, forceMx, color) {
+    this.pos = createVector(x, y);
+    this.vel = createVector();
+    this.acc = createVector();
+    this.mass = 1;
+    this.rad = rad;
+    this.speedMx = speedMx;
+    this.forceMx = forceMx;
+    this.color = color;
+  }
+
+  seek(target) {
+    //현재 내 위치에서 target 위치까지의 거리
+    let desired = p5.Vector.sub(target, this.pos);
+
+    // desired.normalize();
+    // desired.mult(this.speedMx);
+    desired.setMag(this.speedMx);
+    let steering = p5.Vector.sub(desired, this.vel);
+
+    if (debug) {
+      push();
+      translate(this.pos.x, this.pos.y);
+      noFill();
+      stroke(127);
+      line(0, 0, desired.x * 10, desired.y * 10);
+      pop();
+    }
+    steering.limit(this.forceMx);
+    this.applyForce(steering);
+  }
+
+  applyForce(force) {
+    let calcedAcc = p5.Vector.div(force, this.mass);
+    this.acc.add(calcedAcc);
+  }
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+  display() {
+    let angle = this.vel.heading();
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(angle);
+    noStroke();
+    fill(this.color);
+    beginShape();
+    vertex(this.rad, 0);
+    vertex(this.rad * cos(radians(-135)), this.rad * sin(radians(-135)));
+    vertex(0, 0);
+    vertex(this.rad * cos(radians(135)), this.rad * sin(radians(135)));
+    endShape(CLOSE);
+    pop();
+  }
+}
